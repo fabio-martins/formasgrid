108b666da5e337bef8e6173a4db8779d7280bfd3
Fabio Martins <fabio.martins@unila.edu.br>
2025-08-04 22:29:40 -0300

    chore(project): add rails project files

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,17 +14,6 @@ fi
 echo "✅ Migrando banco..."
 rails db:migrate
 
-# Precompile assets apenas se necessário
-if [ "$RAILS_ENV" = "production" ]; then
-  echo "🚀 Pré-compilando assets (produção)..."
-  rails assets:precompile
-else
-echo "🚀 Pré-compilando assets (development)..."
-  rails assets:clobber
-  rails assets:precompile
-  RAILS_ENV=development bundle exec rails assets:precompile
-fi
-
 # Remove o arquivo pid do servidor, se existir
 rm -f tmp/pids/server.pid
 
